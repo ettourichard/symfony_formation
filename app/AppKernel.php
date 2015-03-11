e09ac4dcea7a9e232aee4a132cccaf8773630f73
dangplamvo <dplam.vo@gmail.com>
2015-03-11 15:00:32 +0100

    Màj AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,9 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
 
+            // Dependance
+            new FOS\UserBundle\FOSUserBundle(),
+
             //  Admin
             // The admin requires some twig functions defined in the security
             // bundle, like is_granted
