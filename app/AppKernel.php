73f4c9ba3e8aad380a88a7af1ad6f3b7a85311a3
clementgos <gos.clement@gmail.com>
2015-03-11 15:13:54 +0100

    user bundle init

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Esgi\BlogBundle\BlogBundle(),
+            new Esgi\UserBundle\UserBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
 
