0493114373fd2b2d53e7702697777f0f16a2dfe1
ettourichard <ettou.richard@gmail.com>
2015-03-12 23:42:31 +0100

    modification design

--- src/Esgi/BlogBundle/Form/ProposePostType.php
+++ src/Esgi/BlogBundle/Form/ProposePostType.php
@@ -9,15 +9,26 @@ class ProposePostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title', 'text')
+        $builder->add('title', 'text', array(
+            'attr' => array(
+                    'class' => 'form-control',
+                    'placeholder'   => 'Title', ),
+                ))
                 ->add('body', 'genemu_tinymce')
                 ->add('category', 'entity', array(
                     'class' => 'BlogBundle:Category',
                     'property' => 'name',
                     'expanded' => false,
                     'multiple' => false,
-                ))
-                ->add('save', 'submit', array('label' => 'Propose'));
+                    'attr' => array(
+                        'class' => 'form-control', ),
+                    )
+                )
+                ->add('save', 'submit', array(
+                    'label' => 'Propose',
+                    'attr' => array(
+                        'class' => 'form-control', ),
+                    ));
     }
 
     public function getName()
