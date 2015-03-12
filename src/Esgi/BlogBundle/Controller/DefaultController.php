b902e40d4361d068dc999c46745184afe6a5289a
fozeek <qdeneuve@icloud.com>
2015-03-12 11:17:58 +0100

    fixs use

--- src/Esgi/BlogBundle/Controller/DefaultController.php
+++ src/Esgi/BlogBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Esgi\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Symfony\Component\HttpFoundation\Response;
